c273ab47bbddc49e96a7e8683a2a99b19d959e4f
jochanan <user.name>
2025-09-05 07:22:39 +0200

    Java programs commit

---
+++ Example.java
@@ -0,0 +1,10 @@
+/*
+    This is a simple java program
+ */
+
+class Example {
+    // A java program blabla
+    public static void main(String args[]) {
+        System.out.println("Hello world!");
+    }
+}
